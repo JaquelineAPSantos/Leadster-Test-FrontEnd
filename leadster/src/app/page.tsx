fe9d02f3ec024b805c6a877cb60461842421daaa
JaquelineAPSantos <jaque.aps@hotmail.com>
2023-07-02 15:08:53 -0300

    fix: Adicionado o componente Banner a page.tsx

--- leadster/src/app/page.tsx
+++ leadster/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Banner from "@/components/Header/Banner/Banner";
 import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Banner />
     </main>
   )
 }
