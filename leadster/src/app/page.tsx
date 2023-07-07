2e50ac9e77d6b5f38e6e6b25a5ed0d39b64a345c
JaquelineAPSantos <jaque.aps@hotmail.com>
2023-07-07 17:32:27 -0300

    fix: Adicionado o NavBar a page.tsx

--- leadster/src/app/page.tsx
+++ leadster/src/app/page.tsx
@@ -1,11 +1,13 @@
-import Banner from "@/components/Header/Banner/Banner";
+import Banner from "@/components/Banner/Banner";
 import Header from "@/components/Header/Header";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Banner />
+      <NavBar />
     </main>
   )
 }
